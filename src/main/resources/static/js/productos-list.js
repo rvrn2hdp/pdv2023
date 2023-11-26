e05d06ffb2d5b87f5774ab78632b7853b9ff6352
rvrn2hdp <rvrn2hdp@gmail.com>
2023-11-26 20:16:40 -0300

    clase 2 (sin terminar)

---
+++ src/main/resources/static/js/productos-list.js
@@ -0,0 +1,8 @@
+let table = new DataTable('#tablaProductos', {
+    responsive: true,
+    order: [[2, 'asc']],
+    lengthMenu: [3, 5, 10, 20],
+    language: {
+        url: 'https://cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json'
+    }
+});
